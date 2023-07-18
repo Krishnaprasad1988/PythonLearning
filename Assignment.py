26d4c1c901f5307eaed9903802763122b665b2db
krishna <saran.oct1@gmail.com>
2023-07-18 09:24:26 +0530

    first commit

---
+++ Assignment.py
@@ -0,0 +1,21 @@
+# points = {'d': 1, 'e': 2, 'f': 3}
+# # points['b'] += 1
+# l = ['a', 'b', 'c']
+# p = dict.fromkeys(['a', 'b', 'c'], 0)
+# print(p)
+# r = points | p
+# print(20, sep="->")
+# print(30)
+# a = 3j
+# b = a
+# print(int(b))
+# import sys
+# a = 10
+# a_ = 45
+# print(a, sys.getrefcount(a))
+# i_= input()
+# print(int(i_))
+# help("keywords")
+import keyword
+
+print(keyword.kwlist)
